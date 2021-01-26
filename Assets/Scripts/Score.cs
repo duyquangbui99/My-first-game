9ee6461e2f9d8e0fb3aba060d901c07b402627b6
duyquangbui99 <77463586+duyquangbui99@users.noreply.github.com>
2021-01-25 22:17:17 -0600

    First Commit - Make fisrt game

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    double pp;
+    float playerPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pp = player.position.z + 43.75; // + 43.75 since player porsition at z is - 43.75, set z to 0
+        playerPosition = (float)pp; //convert double to float player position
+        scoreText.text = playerPosition.ToString("0");
+    }
+}
